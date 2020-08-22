7cf76d8eece346e3bb93780dba11de55bf0bd806
Ben Rathbone <ben@blinkidentity.com>
2020-08-22 12:31:58 -0500

    Added optional bollinger bands

--- averages.py
+++ averages.py
@@ -8,11 +8,15 @@ import stock_data
 # When short-term crosses above long-term we get a buy signal.
 # When short-term passes below the longer-term we get a sell signal.
 
-def sma(ticker, rolling_windows=[20, 50], hide_price=False, graph_path=os.path.join(os.getcwd(), "sma")):
-    data = stock_data.get_dataframe_by_ticker(ticker)
+def sma(ticker, rolling_windows=[20, 50], hide_price=False, bollinger=False, graph_path=os.path.join(os.getcwd(), "sma")):
+    data = stock_data.get_dataframe_by_ticker(ticker)[-2*(max(rolling_windows)):]
     rolling_means = [data['Adj Close'].rolling(window=i).mean() for i in rolling_windows]
     for i, rolling_mean in enumerate(rolling_means):
         plt.plot(data['Date'], rolling_mean, label=f'{ticker} {rolling_windows[i]} Simple Moving Average')
+    if bollinger is True:
+        data = bollinger_bands(data, window=rolling_windows[0])
+        plt.plot(data['Date'], data['Lower Bollinger'], label=f'{ticker} {rolling_windows[0]} Lower Bollinger Band')
+        plt.plot(data['Date'], data['Upper Bollinger'], label=f'{ticker} {rolling_windows[0]} Upper Bollinger Band')
     if hide_price is False:
         plt.plot(data['Date'], data['Adj Close'], label=f'{ticker} {max(rolling_windows)} Price', color='magenta')
     plt.legend(loc='upper left')
@@ -36,8 +40,18 @@ def ema(ticker, rolling_windows=[20, 50], hide_price=False, graph_path=os.path.j
         os.makedirs(graph_path)
         plt.savefig(os.path.join(graph_path, f'{ticker}_{datetime.datetime.now().date().isoformat()}.png'), format="png")
 
+def bollinger_bands(data, window=20):
+    moving_average = data['Adj Close'].rolling(window=window).mean()
+    std = data['Adj Close'].rolling(window=window).std()
+    data['Upper Bollinger'] = moving_average + (2 * std)
+    data['Lower Bollinger'] = moving_average - (2 * std)
+    return data
+
 def check_for_cross(ticker, windows={"short": 20, "long": 50}):
     data = stock_data.get_dataframe_by_ticker(ticker)
     short_term_mean = data['Adj Close'].rolling(window=windows['short']).mean()
     long_term_mean = data['Adj Close'].rolling(window=windows['long']).mean()
 
+
+if __name__ == '__main__':
+    sma("SLV", bollinger=True)
